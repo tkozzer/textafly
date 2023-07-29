f608b66d7e269cd34053fdd72fc466b0bd20820c
Tyler Kozlowski <Tyler.J.Kozlowski@gmail.com>
2023-07-29 13:24:41 -0500

    initialize project

---
+++ src/main/java/dev/tkoz/textafly/TextaflyApplication.java
@@ -0,0 +1,13 @@
+package dev.tkoz.textafly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextaflyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextaflyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tkoz/textafly/TextaflyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tkoz.textafly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextaflyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
